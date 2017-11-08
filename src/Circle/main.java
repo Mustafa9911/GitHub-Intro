d6deeb3096c1e4b34311ab21e1309475da7aad04
Mustafa991 <mustafa.abbas.te15c@gmail.com>
2017-11-08 13:45:07 +0100

    Jag gjorde cirkeln kontrollerbar.

--- src/Circle/main.java
+++ src/Circle/main.java
@@ -3,11 +3,14 @@ package Circle;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class main extends Application{
 
+
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
@@ -16,9 +19,38 @@ public class main extends Application{
         Scene scene = new Scene(root, 400, 300);
 
         
-        Circle cir = new Circle(200,150,15);
+   	 Circle cir = new Circle(200,150,15);
+        
         root.getChildren().add(cir);
         
+        
+        scene.setOnKeyPressed(event ->{
+        	
+        	
+        	if(event.getCode() == KeyCode.W){
+        		
+        		cir.setTranslateY(cir.getTranslateY()-3);
+        		
+        	}else if(event.getCode() == KeyCode.S){
+        		
+        		cir.setTranslateY(cir.getTranslateY()+3);
+        		
+        	}else if(event.getCode() == KeyCode.A){
+            		
+            		cir.setTranslateX(cir.getTranslateX()-3);
+            		
+            	}else if(event.getCode() == KeyCode.D){
+            		
+            		cir.setTranslateX(cir.getTranslateX()+3);
+            		
+            	}
+        		
+        		
+        		
+        	
+        	
+        	
+        });
         
         primaryStage.setTitle("My JavaFX Application");
         primaryStage.setScene(scene);
