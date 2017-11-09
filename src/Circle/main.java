a666f7cafa5609a0dd2a640b2135f7d25274a1a6
Mustafa991 <mustafa.abbas.te15c@gmail.com>
2017-11-09 17:53:55 +0100

    Visar vilka knappar som trycks

--- src/Circle/main.java
+++ src/Circle/main.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class main extends Application{
@@ -15,7 +17,7 @@ public class main extends Application{
 	boolean pressingS = false;
 	boolean pressingA = false;
 	boolean pressingD = false;
-	
+	String pressingKeyStr="";
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -29,17 +31,42 @@ public class main extends Application{
         cir.setTranslateX(200);
         cir.setTranslateY(150);
         
-        root.getChildren().add(cir);
+        Text text = new Text(pressingKeyStr);
+        text.setFont(Font.font(30));
+        text.setFill(Color.BLUE);
+        text.setTranslateY(25);
+        
+        root.getChildren().addAll(cir,text);
+        
+       
         
         AnimationTimer t = new AnimationTimer(){
 
 			@Override
 			public void handle(long arg0) {
-				
-				if(pressingW) cir.setTranslateY(cir.getTranslateY()-3);
-				if(pressingS) cir.setTranslateY(cir.getTranslateY()+3);
-				if(pressingA) cir.setTranslateX(cir.getTranslateX()-3);
-				if(pressingD) cir.setTranslateX(cir.getTranslateX()+3);
+			
+
+				if(pressingW) {
+					cir.setTranslateY(cir.getTranslateY()-3);
+					pressingKeyStr += "UP ";
+				}
+				if(pressingS){ 
+					cir.setTranslateY(cir.getTranslateY()+3);
+					pressingKeyStr += "DOWN ";
+				}
+				if(pressingA){ 
+					cir.setTranslateX(cir.getTranslateX()-3);
+					pressingKeyStr += "LEFT ";
+
+				}
+				if(pressingD){ 
+					cir.setTranslateX(cir.getTranslateX()+3);
+					pressingKeyStr += "RIGHT ";
+
+				}
+				text.setText(pressingKeyStr);
+				pressingKeyStr="";
+
 				
         
 	        scene.setOnKeyPressed(event ->{
